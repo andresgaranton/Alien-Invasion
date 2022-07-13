dd96df7473ad0da8ef883b21895383def80bbc21
Andres Garanton <77209820+andresgaranton@users.noreply.github.com>
2022-07-13 10:04:23 -0600

    Added files via uplaod

---
+++ settings.py
@@ -0,0 +1,28 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion."""
+
+	def __init__(self):
+		"""initialize the game's settings."""
+		# screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230 , 230)
+
+		# Ship settings
+		self.ship_speed = 1.5
+		self.ship_limit = 1
+
+		#Alien Settings
+		self.alien_speed = 0.1
+		self.fleet_drop_speed = 50
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = -1 
+
+		# Bullet Settings
+		self.bullet_speed = 1.5
+		self.bullet_width = 300
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 3
+
+
